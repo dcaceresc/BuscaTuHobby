576fab265a7fa481111c5d973a072cc8488dc90b
Diego Cáceres <mavroslykanthropos@outlook.com>
2025-08-04 22:28:01 -0400

    Actualización Angular 20

--- src/Web/WebUI/src/app/app.config.ts
+++ src/Web/WebUI/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
-import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom, provideZonelessChangeDetection } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AuthorizeInterceptor } from "./core/interceptors/authorize.interceptor";
 import { ErrorInterceptor } from "./core/interceptors/error.interceptor";
@@ -8,7 +8,7 @@ import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 export const appConfig : ApplicationConfig = {
     providers: [
-        provideExperimentalZonelessChangeDetection(),
+        provideZonelessChangeDetection(),
         provideRouter(routes,withComponentInputBinding()),
         importProvidersFrom(FontAwesomeModule),
         provideHttpClient(withFetch(),withInterceptors([AuthorizeInterceptor,ErrorInterceptor]))
